95fbf862f7d6e074cce53c15c72c29a641f0b7ef
lonex <gder03818@gmail.com>
2026-07-08 12:29:49 +0300

    Update deleteormove.py
    version 0.1

--- deleteormove.py
+++ deleteormove.py
@@ -3,18 +3,19 @@ import time
 import os
 import shutil
 
-in1 = input("select path:")
-in2 = os.listdir(in1)
+
+path = "/home/"
+in1 = os.listdir(path)
 
 sel = int(input("Select your choice(1 to move or 2 to delete): "))
 if sel == 1:
-    p1th = [in2]
+    p1th = [in1]
     print(p1th)
     select = int(input("Select number (0 - infinity): "))
     targetfiles1 = in1[select]
     shutil.move(f"/home/lonex/Downloads/{targetfiles1}", "/home/lonex/Videos/")
 elif sel == 2:
-    p2th = [in2]
+    p2th = [in1]
     print(p2th)
     print("Предупреждение: после выбора файл будет удален!")
     time.sleep(2)

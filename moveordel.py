cbdb92f8e9da8a06d24dd32e75a9fbdcc79b3073
lonex <gder03818@gmail.com>
2026-07-09 15:20:28 +0300

    Add files via upload

---
+++ moveordel.py
@@ -0,0 +1,81 @@
+import datetime
+import time
+import os
+import shutil
+
+
+
+in1 = input("select files path:")
+in2 = os.listdir(in1)
+def print_logo():
+    logo = """
+                                             #**+===+*##                                        
+                                 ##+=-*%#+:... ............:=#                          
+                             #+=:..+%%*-......................:+                        
+                           #-...:*%%%=.  ........................-#                     
+                          #-...*@%%#+=:....::..:===+==-:...........-                    
+                          *:.=%%%%%%#*+-:=##%%%%%%%%%%%%%%#=.........=                  
+                          +.+%%%%%%%%%##%%%%%%%%#+-::.....:*%%=........*                
+                          +*%%%%%%%%%%%%%%%%*-...............-#%=.... ..=               
+                          *%%%%%%%%%%%%%%+:..........  ........=%#:.....:*              
+                                       #-. =%%%*-......=*%%%*-....#%:.....+             
+                                     #-...=%%%%%%  ...=%%%%%%%+....%%.....+             
+                                     ....+%%%%%%%%....+%%%%%%%%....:%+....*             
+                                    -. .  №%%%%%%......#%%%%%%+.....*%...:#             
+                                    .......-#%%*-.......-#%%*-......+%=--=              
+                                    .................. ........  ..*%**##              
+                                  #%%-...............................#                  
+                             #+++==#%#.........          .....:.....                 
+                             #-:...=%%+.....:--:.. . ...     ......                    
+                             #-.....=#%+........... .     ........:#                    
+                             #-.......=%*.......         ........-%##                   
+                              *. .......#%+: ...          .....-#%+-=*#                 
+                               #.........:*%%=.............:=#%%=...:+                  
+                                 -.....  . .-*%%%*=----=*%%%%+: ....+                   
+                                  #:.............-=+++++=-........:*                    
+                                    #:..........         ........=#                     
+                                        +:.... ..         ......-*                      
+                                           #=:.............:=+#                         
+                                           #**#%%%%%%%%%%%%%%%**#                       
+                                             #+-...::::::..:=#                          
+                                               #*=:....:=*#                             
+                                                   ##
+
+    """
+    print(logo)
+print_logo()
+sel = int(input("Select your choice(1 to move or 2 to delete): "))
+if sel == 1:
+    sel2 = int(input("Select your choice(1 to move file or 2 to move whole directory with all files): "))
+    timee = datetime.datetime.now()
+    datee = timee.strftime("%y/%m/%d/ %H:%M")
+    print(datee)
+    if sel2 == 1:
+        p1th = [in2]
+        print(p1th)
+        select = int(input("Select number (0 - infinity): "))
+        targetfiles1 = in2[select]
+        dst = input("Enter destination path: ")
+        src = in1
+        final_destination = os.path.join(dst, targetfiles1)
+        shutil.move(os.path.join(src, targetfiles1), final_destination)
+        print("file moved")
+    elif sel2 == 2:
+        directory = input("please input the directory path:")
+        des = input("please input the destination path:")
+        shutil.copytree(directory,des)
+        shutil.rmtree(directory)
+elif sel == 2:
+    timee2 = datetime.datetime.now()
+    datee2 = timee2.strftime("%y/%m/%d/ %H:%M")
+    print(datee2)
+    p2th = [in2]
+    print(p2th)
+    time.sleep(1)
+    select = int(input("Select number (0 - infinity): "))
+
+    targetfiles2 = in2[select]
+    os.remove(f"/home/lonex/Downloads/{targetfiles2}")
+    print("file deleted")
+else:
+    print("Please select a valid option")
